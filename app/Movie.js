f200168b73954a4a2ac31a540f8980d04b71adfe
saifulalamfahime <you@example.com>
2023-04-08 20:22:41 +0600

    done

---
+++ app/Movie.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+// import Image from "next/image";
+
+export default function Movie({ title, id , poster_path, release_date }) {
+    const imagePath = "https://image.tmdb.org/t/p/original"
+
+    console.log(poster_path);
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{release_date}</h2>
+      <Link href={`/${id}`}>
+        <img src={imagePath + poster_path} alt={title} width={200} />
+     </Link>
+    </div>
+  )
+}
---
+++ app/[movie]/page.js
@@ -0,0 +1,18 @@
+
+
+export default async function movieDetails({params}) {
+    const {movie} =params;
+    const imagePath = "https://image.tmdb.org/t/p/original"
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}popular?api_key=68234cd06a5dcf3abf2890260729cfa0`)
+    const res = await data.json()
+  return (
+    <div>
+      <h1 className="text-2xl">{res.title}</h1>
+      <h2 className="texy-lg">{res.release_date}</h2>
+      <h2>Runtime: {res.runtime} minutes</h2>
+      <h2 className="bg-green-600 inline-block my-2 py-2 px-4 rounded">{res.status}</h2>
+      <img className="my-6 " src={imagePath + res.backdrop_path} width={600}/>
+      <p>{res.overview}</p>
+    </div>
+  )
+}
